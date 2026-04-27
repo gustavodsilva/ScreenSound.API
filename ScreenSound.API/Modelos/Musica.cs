8dd6c63cc6a74fa49882db5f49f021337d3698a0
gustavodsilva <gustavo.gsilva83@gmail.com>
2026-04-27 18:37:39 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound.API/Modelos/Musica.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSound.API.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração em segundos: {Duracao/1000}");
+        Console.WriteLine($"Gênero: {Genero}");
+    }
+}
---
+++ ScreenSound.API/Program.cs
@@ -0,0 +1,25 @@
+﻿using ScreenSound.API.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
+        if (musicas != null)
+        {
+            foreach (var musica in musicas)
+            {
+                musica.ExibirDetalhes();
+                Console.WriteLine();
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+    
+}
